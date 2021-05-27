a35b90f54c672365cc38bd2b46e48c2219a65c6a
jaronsj <jaronsj@byui.edu>
2021-05-27 10:28:14 -0600

    added hero image and finished wind chill javascript

--- lesson06/assingment/js/windchill.js
+++ lesson06/assingment/js/windchill.js
@@ -1,13 +1,20 @@
-window.addEventListener("load", (event)=>{
-    function doInputOutput(){
-        let temp= parseFloat(document.getElementById("temp").value);
-        let wind_speed= parseFloat(document.getElementById("wind").value);
-        let wind_chill= windChill(temp, wind_speed);
-        
-        document.getElementById("wind_chill").innerHTML= wind_chill;
+window.addEventListener('load', () => {
+    const calWindChill = (temperature, speed) => {
+        return (temperature >= 50 && speed > 3) 
+        ?
+            Math.round(
+                35.74 + (0.6215 * temperature) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * (temperature
+                * Math.pow(speed, 0.16)))
+        ,):
+            "N/A";
     }
-    function windChill(tempF, speed){
-        let f= 35.74+ (0.6215* tempF)- (35.75* (speed**0.16)) + (0.4275 * tempF *(speed ** 0.16));
-        return Math.round(f);
+
+    const displayWindChill = () => {
+        let temperature = Number(document.getElementById("temp").textContent || 0);
+        let wind = Number(document.getElementById("wind").textContent || 0);
+        let result = calWindChill(temperature, wind);
+        document.getElementById("wind_chill").innerHTML = result;
     }
+
+    displayWindChill();
 })

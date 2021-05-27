46e9c6aa91c05604f4551f1f50eb2a8756a69b40
jaronsj <jaronsj@byui.edu>
2021-05-27 07:48:36 -0600

    wind chill update

---
+++ lesson06/assingment/js/windchill.js
@@ -0,0 +1,11 @@
+function doInputOutput(){
+    let temp= parseFloat(document.getElementById("temp").value);
+    let wind_speed= parseFloat(document.getElementById("wind").value);
+    let wind_chill= windChill(temp, wind_speed);
+    
+    document.getElementById("wind_chill").innerHTML= wind_chill;
+}
+function windChill(tempF, speed){
+    let f= 35.74+ (0.6215* tempF)- (35.75* (speed**0.16)) + (0.4275 * tempF *(speed ** 0.16));
+    return Math.round(f);
+}
